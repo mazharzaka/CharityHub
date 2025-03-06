38be0037ccf61838b4884bd30638c4c166e487e6
mazharzaka <mazharmohamed566@gmail.com>
2025-03-06 14:33:33 +0200

    add campaign model and donation model

---
+++ app/src/models/Campaign.model.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const CampaignSchema = new mongoose.Schema({    
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    image:{type:String},
+    goalAmount:{
+        type:Number,
+        required:true
+    },
+    currentAmount:{
+        type:Number,
+        default:0
+    },  
+    currency: { enum: ['USD', 'EGP'], default: 'EGP' },
+    status: { type: String, enum: ['pending', 'active', 'completed', 'failed'], default: 'pending' },
+    startDate: { type: Date, default: Date.now },
+    endDate: { type: Date, required: true },
+    category: { type: String, enum: ['health', 'education', 'environment', 'human-rights', 'animals', 'disaster-relief', 'others'], default: 'others' },
+    location: {
+        latitude: { type: Number, required: true },
+        longitude: { type: Number, required: true },
+        address: { type: String, required: true }
+      },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    donations: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Donation'
+    }],
+    createdAt : {
+        type: Date,
+        default: Date.now
+    }
+    ,updatedAt : {
+        type: Date,
+        default: Date.now
+    },
+isFeatured:{
+    type:Boolean,
+    default:false
+},
+	volunteers:{
+        type:[{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+
+    }, 
+    tags: {
+        type: [String]
+    },
+    impactReport :{
+        type:String,
+        default:""
+    }
+})
+const Campaign = mongoose.model('Campaign', CampaignSchema);
+module.exports = Campaign;
---
+++ app/src/models/Donation.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const DonationSchema = new mongoose.Schema({
+    donorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    currency: { 
+        type: String, 
+        enum: ['USD', 'EGP'], 
+        default: 'EGP' 
+    },
+    donationType: { 
+        type: String, 
+        enum: ['one-time', 'monthly'], 
+        default: 'one-time' 
+    },
+    paymentMethod: { 
+        type: String, 
+        enum: ['credit-card', 'paypal', 'cash'], 
+        default: 'cash' 
+    },
+    status: { 
+        type: String, 
+        enum: ['pending', 'completed', 'failed', 'refund'], 
+        default: 'pending' 
+    },
+    message: {
+        type: String,
+        trim: true
+    },
+ 
+    anonymous: {
+        type: Boolean,
+        default: false
+    },
+    transactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Transaction',
+        default: null
+    }
+}, { timestamps: true });
+
+const Donation = mongoose.model('Donation', DonationSchema);
+module.exports = Donation;
